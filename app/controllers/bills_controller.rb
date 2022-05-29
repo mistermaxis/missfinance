b953f1b2e12eec3cbc89dae2294e18c43a7c9296
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-29 18:57:04 -0300

    Fix rubocop errors

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class BillsController < ApplicationController
   def index; end
 
--- test/controllers/bills_controller_test.rb
+++ test/controllers/bills_controller_test.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require 'test_helper'
 
 class BillsControllerTest < ActionDispatch::IntegrationTest
