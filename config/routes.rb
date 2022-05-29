3e346ce5214e237d16a983da789e4c34014c193d
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-29 19:22:40 -0300

    Generate schema

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get 'bills/index'
+  get 'bills/new'
+  get 'bill_groups/index'
+  get 'bill_groups/new'
+  get 'bill_group/index'
+  get 'bill_group/new'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'bill_groups#index'
 end
--- db/migrate/20220529215314_create_bills.rb
+++ db/migrate/20220529215314_create_bills.rb
@@ -1,7 +1,6 @@
 class CreateBills < ActiveRecord::Migration[7.0]
   def change
     create_table :bills do |t|
-      t.references :user, foreign_key: 'author_id'
       t.text :name
       t.float :amount
 
