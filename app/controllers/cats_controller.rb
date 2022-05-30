0ea2c6d18474d740c440f3011ec8c7b86c49030d
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-30 19:19:06 -0300

    Add controller methods for categories

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,5 +1,31 @@
 class CatsController < ApplicationController
-  def index; end
+  before_action :authenticate_user!
 
-  def new; end
+  def index
+    @categories = Cat.where(author: current_user)
+  end
+
+  def show
+    @category = Cat.find(params[:id])
+  end
+
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    @category = Cat.new(cat_params)
+
+    if @category.save
+      redirect_to cat_path(@category)
+    else
+      redirect_to cats_path
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :icon)
+  end
 end
