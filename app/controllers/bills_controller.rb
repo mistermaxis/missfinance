23a0438c14d7d8a38d041eac3c9964e1dc1356e2
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-30 19:18:50 -0300

    Add controller methods for payments

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,5 +1,25 @@
 class BillsController < ApplicationController
-  def index; end
+  before_action :authenticate_user!
 
-  def new; end
+  def new
+    @bill = Bill.new
+  end
+
+  def create
+    @bill = Bill.new(bill_params)
+    @bill.author = current_user
+    @bill.cats.append(Cat.first)
+
+    if @bill.save
+      redirect_to bill_path(@bill)
+    else
+      redirect_to bills_path
+    end
+  end
+
+  private
+
+  def bill_params
+    params.require(:bill).permit(:name, :amount)
+  end
 end
