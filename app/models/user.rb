95b21c5751e6e9df72b908b64985e0ca4a0af3f8
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-30 18:04:30 -0300

    Add validation to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bills, inverse_of: :author, foreign_key: 'author_id'
+  has_many :cats, inverse_of: :author, foreign_key: 'author_id'
+
+  validates :name, presence: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 end
