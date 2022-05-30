a6e303462a04c0b1dcf5ac91b0c080d63e375210
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-30 13:15:36 -0300

    Restructure database schema

--- db/migrate/20220529215225_create_cats.rb
+++ db/migrate/20220529215225_create_cats.rb
@@ -1,8 +1,7 @@
 class CreateCats < ActiveRecord::Migration[7.0]
   def change
     create_table :cats do |t|
-      t.text :name
-      t.text :icon
+      t.string :name, null: false
 
       t.timestamps
     end
