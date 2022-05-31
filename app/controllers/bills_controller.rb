bfb57cead6ccd96e86cf42fe70d915df14c6c841
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-05-31 14:56:32 -0300

    Update create method

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -12,16 +12,16 @@ class BillsController < ApplicationController
     @bill.author = current_user
 
     @cat_ids = bill_params[:cat_bill][:cat_id].select do |id|
-      !id.empty?
+      id.present?
     end
     
     @categories = Cat.where(id: @cat_ids)
     
     if @bill.save
       @bill.cats << @categories
-      redirect_to cat_path(@cat_ids.first)
+      redirect_to cat_path(@cat_ids.first), notice: "Transaction \"#{@bill.name}\" was created successfully"
     else
-      redirect_to cats_path
+      redirect_to cats_path, notice: "There was an error while saving transatcion \"#{@bill.name}\""
     end
 
   end
