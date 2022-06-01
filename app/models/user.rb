94eca61d53e5da36615416b8c6a1bbef192b8792
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-06-01 18:33:52 -0300

    Update user to confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :bills, inverse_of: :author, foreign_key: 'author_id'
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+  
+         has_many :bills, inverse_of: :author, foreign_key: 'author_id'
   has_many :cats, inverse_of: :author, foreign_key: 'author_id'
 
   validates :name, presence: true
-
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
 end
