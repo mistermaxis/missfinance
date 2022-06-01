4bda8f410524381e5d22cb51f9ca697f7c8bdb8f
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-06-01 10:17:14 -0300

    Update succes/failure routes to take id

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   
   get 'home/index'
-  get 'notices/success'
-  get 'notices/failure'
+  get 'notices/success/:id', to: 'notices#success', as: 'success'
+  get 'notices/failure/:id', to: 'notices#failure', as: 'failure'
 
   resources :cats, only: [:index, :show, :new, :create]
   resources :bills, only: [:new, :create]
