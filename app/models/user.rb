0fdd9ab1b2d4105da845c281c13ae3f254a46dbb
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-06-02 14:25:47 -0300

    Update alignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  
-         has_many :bills, inverse_of: :author, foreign_key: 'author_id'
+
+  has_many :bills, inverse_of: :author, foreign_key: 'author_id'
   has_many :cats, inverse_of: :author, foreign_key: 'author_id'
 
   validates :name, presence: true
