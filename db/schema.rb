3957156966429c0929349bfb4d21727f497a7cca
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-06-15 13:49:49 -0300

    Update devise to confirm email

--- db/schema.rb
+++ db/schema.rb
@@ -72,8 +72,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_30_202237) do
     t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
