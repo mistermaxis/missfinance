c6ddceb2b5c41eaecaebfa316468f3d015b9dc89
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2022-06-16 16:09:44 -0300

    Add devise user controllers routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+  }
   
   get 'home/index'
   
